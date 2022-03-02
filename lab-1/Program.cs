7342a879be72148b1783d607cba4157932e9edfb
michal.mierzwa <michal.mierzwa@PK3-11>
2022-03-02 18:33:02 +0100

    test

--- lab-1/Program.cs
+++ lab-1/Program.cs
@@ -6,7 +6,49 @@ namespace lab_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Person person = Person.OfName("Adam");
+            Console.WriteLine(person.FirstName);
+        }
+    }
+
+    public class Person
+    {
+        private string firstName;
+
+        private Person(string _firstName)
+        {
+            firstName = _firstName;
+        }
+
+        public static Person OfName(string name)
+        {
+            if(name != null && name.Length >= 2)
+            {
+                return new Person(name);
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("Imię zbyt krótkie");
+            }
+        }
+
+        public string FirstName
+        { 
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                if(value != null && value.Length >= 2)
+                {
+                    firstName = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Niepoprawne imię");
+                }
+            }
         }
     }
 }
