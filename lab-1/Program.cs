060e5de2516910e053c6b945792808b205c3c287
Michał Mierzwa <Michal.Mierzwa@microsoft.wsei.edu.pl>
2022-03-10 18:54:12 +0100

    added student and tank class

--- lab-1/Program.cs
+++ lab-1/Program.cs
@@ -6,38 +6,173 @@ namespace lab_1
     {
         static void Main(string[] args)
         {
-            Person person = Person.OfName("Adam");
-            Console.WriteLine(person.FirstName);
-            Money money = Money.Of(12, Currency.PLN) ?? Money.Of(0, Currency.PLN);
-            Money result = 0.22m * money;
-            Console.WriteLine(result.Value);
-            money = Money.Of(5, money.Currency) + Money.Of(10, money.Currency);
-            Console.WriteLine(money.Value);
+            //Person person = Person.OfName("Adam");
+            //Console.WriteLine(person.FirstName);
+            //Money money = Money.Of(12, Currency.PLN) ?? Money.Of(0, Currency.PLN);
+            //Money result = 0.22m * money;
+            //Console.WriteLine(result.Value);
+            //money = Money.Of(5, money.Currency) + Money.Of(10, money.Currency);
+            //Console.WriteLine(money.Value);
 
-            //decimal price = money;
-            double cost = (double) money;
-            float c = (float) money;
+            ////decimal price = money;
+            //double cost = (double) money;
+            //float c = (float) money;
 
-            Console.WriteLine(money.Equals(Money.Of(12, Currency.PLN)));
+            //Console.WriteLine(money.Equals(Money.Of(12, Currency.PLN)));
 
-            IEquatable<Money> ie = money;
+            //IEquatable<Money> ie = money;
 
-            Money[] prices =
+            //Money[] prices =
+            //{
+            //    Money.Of(52, Currency.USD),
+            //    Money.Of(100, Currency.PLN),
+            //    Money.Of(51, Currency.EUR),
+            //    Money.Of(30, Currency.EUR),
+            //    Money.Of(99, Currency.PLN),
+            //    Money.Of(45, Currency.USD)
+            //};
+
+            //Array.Sort(prices);
+
+            //foreach(var p in prices)
+            //{
+            //    Console.WriteLine(p);
+            //}
+
+            Tank tank1 = new Tank(100);
+            Tank tank2 = new Tank(200);
+
+            tank1.refuel(100);
+            tank2.refuel(100);
+
+            Console.WriteLine(tank2);
+            Console.WriteLine(tank1);
+
+            Console.WriteLine(tank2.refuel(tank1, 100));
+
+            Console.WriteLine(tank2);
+            Console.WriteLine(tank1);
+
+
+        }
+    }
+
+    class Student: IComparable<Student>
+    {
+        public string Nazwisko { get; set; }
+        public string Imie { get; set; }
+        public decimal Srednia { get; set; }
+
+        public int CompareTo(Student other)
+        {
+            int curSurname = Nazwisko.CompareTo(other.Nazwisko);
+            if (curSurname == 0)
             {
-                Money.Of(52, Currency.USD),
-                Money.Of(100, Currency.PLN),
-                Money.Of(51, Currency.EUR),
-                Money.Of(30, Currency.EUR),
-                Money.Of(99, Currency.PLN),
-                Money.Of(45, Currency.USD)
-            };
-
-            Array.Sort(prices);
-
-            foreach(var p in prices)
-            {
-                Console.WriteLine(p);
+                int curName = Imie.CompareTo(other.Imie);
+                if(curName == 0)
+                    return Srednia.CompareTo(other.Srednia);
+                else
+                    return curName;
             }
+            else
+                return curSurname;
+        }
+    }
+
+    public class Tank
+    {
+        public readonly int Capacity;
+        private int _level;
+        public Tank(int capacity)
+        {
+            Capacity = capacity;
+        }
+        public int Level
+        {
+            get
+            {
+                return _level;
+            }
+            private set
+            {
+                if (value < 0 || value > Capacity)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                _level = value;
+            }
+        }
+
+        public bool refuel(Tank sourceTank, int amount)
+        {
+            if (amount <= 0)
+                return false;
+            if(_level + amount <= Capacity && amount <= sourceTank.Level)
+            {
+                sourceTank.consume(amount);
+                this.refuel(amount);
+                return true;
+            }
+            return false;
+        }
+
+
+
+        public bool refuel(int amount)
+        {
+            if (amount < 0)
+            {
+                return false;
+            }
+            if (_level + amount > Capacity)
+            {
+                return false;
+            }
+            _level += amount;
+            return true;
+        }
+
+        //public void refuel(int amount)
+        //{
+        //    if (amount < 0)
+        //    {
+        //        throw new ArgumentException("Argument cant be non positive!");
+        //    }
+        //    if (_level + amount > Capacity)
+        //    {
+        //        throw new ArgumentException("Argument is to large!");
+        //    }
+        //    _level += amount;
+        //}
+
+        //public int refuel(int amount)
+        //{
+        //    if (amount < 0 || _level == Capacity)
+        //    {
+        //        return 0;
+        //    }
+        //    if (_level + amount > Capacity)
+        //    {
+        //        int result = Capacity - _level;
+        //        _level = Capacity;
+        //        return result;
+        //    }
+        //    _level += amount;
+        //    return amount;
+        //}
+        public bool consume(int amount)
+        {
+            if (amount <= 0)
+                return false;
+            if (amount > _level)
+                return false;
+            _level -= amount;
+            return true;
+        }
+
+        public override string ToString()
+        {
+            return $"Tank: Capacity = {Capacity}, Level = {_level}";
         }
     }
 
@@ -208,5 +343,7 @@ namespace lab_1
             else
                 return curResult;
         }
+
+        
     }
 }
--- lab-3/Program.cs
+++ lab-3/Program.cs
@@ -8,5 +8,159 @@ namespace lab_3
         {
             Console.WriteLine("Hello World!");
         }
+
+        /// <summary>
+        /// zad 2
+        /// </summary>
+
+        class Beam
+        {
+            private float _width;
+            private float _length;
+
+            public float width
+            {
+                get { return _width; }
+                set
+                {
+                    if (value <= 0)
+                        throw new ArgumentOutOfRangeException("Width cannot be less than 0");
+                    _width = value;
+                }
+            }
+
+            public float length
+            {
+                get { return _length; }
+                set
+                {
+                    if (value <= 0)
+                        throw new ArgumentOutOfRangeException("Length cannot be less than 0");
+                    _length = value;
+                }
+            }
+
+            public Beam(float beamWidth, float beamLength)
+            {
+                length = beamLength;
+                width = beamWidth;
+            }
+
+            public bool CutBeam(float cutLength)
+            {
+                if (cutLength > length)
+                    return false;
+                length -= cutLength;
+                return true;
+            }
+
+            public bool isEmpty()
+            {
+                return length == 0;
+            }
+
+            public float getBeamField()
+            {
+                return (float)length * width;
+            }
+
+        }
+
+        /// <summary>
+        /// Zad 1
+        /// </summary>
+
+        public class Coin
+        {
+            /// <summary>
+            /// Creates new Coin object with value of 1
+            /// </summary>
+            /// <returns>New Coin object with value of 1</returns>
+            public static Coin one()
+            {
+                return new Coin(1);
+            }
+
+            /// <summary>
+            /// Creates new Coin object with value of 2
+            /// </summary>
+            /// <returns>New Coin object with value of 2</returns>
+            public static Coin two()
+            {
+                return new Coin(2);
+            }
+
+            /// <summary>
+            /// Creates new Coin object with value of 5
+            /// </summary>
+            /// <returns>New Coin object with value of 5</returns>
+            public static Coin five()
+            {
+                return new Coin(5);
+            }
+
+            private int _number;
+
+            public int number
+            {
+                get { return _number; }
+            }
+            private Coin(int number)
+            {
+                _number = number;
+            }
+
+            /// <summary>
+            /// Sums two coin object and returns addition result
+            /// </summary>
+            /// <param name="a">Coin object</param>
+            /// <param name="b">Coin object</param>
+            /// <returns>Addidion result</returns>
+            public static int operator +(Coin a, Coin b)
+            {
+                return (int)a.number + b.number;
+            }
+
+            /// <summary>
+            /// Returns table of coin objects whose sum of denominations is equal to the parameter with total value
+            /// </summary>
+            /// <param name="value">integer total value</param>
+            /// <returns>Table of coin objects</returns>
+            /// <exception cref="ArgumentException">throw for argument less than 0</exception>
+
+            public static Coin[] Of(int value)
+            {
+                if (value < 0)
+                    throw new ArgumentException("Given value cannot be lower than 0");
+
+                List<Coin> coinList = new List<Coin>();
+
+                while (value >= 0)
+                {
+                    if (value >= 5)
+                    {
+                        coinList.Add(Coin.five());
+                        value -= 5;
+                        continue;
+                    }
+
+                    if (value >= 2)
+                    {
+                        coinList.Add(Coin.two());
+                        value -= 2;
+                        continue;
+                    }
+
+                    if (value >= 1)
+                    {
+                        coinList.Add(Coin.one());
+                        value -= 1;
+                        continue;
+                    }
+                }
+
+                return coinList.ToArray();
+            }
+        }
     }
 }
